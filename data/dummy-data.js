6fd1ab0f46dbc5a8ac2a5a7cc99fa30784539679
ahmet-gunes <ahmetgunes.ceng@gmail.com>
2023-09-28 13:30:51 +0300

    update

---
+++ data/dummy-data.js
@@ -0,0 +1,32 @@
+export const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 45.55,
+        date: new Date('2023-09-24'),
+    },
+    {
+        id: 'e2',
+        description: 'A pair of trousers',
+        amount: 11.23,
+        date: new Date('2022-04-19'),
+    },
+    {
+        id: 'e3',
+        description: 'Some bananas',
+        amount: 4.56,
+        date: new Date('2021-03-22'),
+    },
+    {
+        id: 'e4',
+        description: 'A book',
+        amount: 14.99,
+        date: new Date('2020-02-01'),
+    },
+    {
+        id: 'e5',
+        description: 'A gamepad',
+        amount: 55.99,
+        date: new Date('2023-11-23'),
+    },
+]
